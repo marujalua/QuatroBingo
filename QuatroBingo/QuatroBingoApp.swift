3795d9fc18bb6a042ecb1c55b16ccd29f21f6b52
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2025-05-29 23:40:14 -0300

    Initial Commit

---
+++ QuatroBingo/QuatroBingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuatroBingoApp.swift
+//  QuatroBingo
+//
+//  Created by Lua Ferreira de Carvalho on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct QuatroBingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuatroBingoTests/QuatroBingoTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuatroBingoTests.swift
+//  QuatroBingoTests
+//
+//  Created by Lua Ferreira de Carvalho on 29/05/25.
+//
+
+import Testing
+@testable import QuatroBingo
+
+struct QuatroBingoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
