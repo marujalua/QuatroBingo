c8714a32fa6d1c487025aa2bd46ccbf2356449ad
Marcos Chevis Meira <marcoschevism@gmail.com>
2025-06-07 16:01:11 -0300

    fix divider position

--- QuatroBingo/Features/Bingo/Presentation/Score/ScoreListAdapter.swift
+++ QuatroBingo/Features/Bingo/Presentation/Score/ScoreListAdapter.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum ScoreListAdapter {
     static func adapt(adapt data: [String: Player]) -> [ScoreView.ViewData.ScoreItem] {
-        let dividerPosition = 2
+        let dividerPosition = 3
         var result = data.values.sorted { p1, p2 in
             p1.score > p2.score
         }.enumerated().map { player in
@@ -19,13 +19,13 @@ enum ScoreListAdapter {
                     emoji: player.element.emoji,
                     name: player.element.name,
                     score: player.element.score.formatted(),
-                    isHighlighted: player.offset < dividerPosition + 1,
+                    isHighlighted: player.offset < dividerPosition,
                     place: (player.offset + 1).formatted()
                 )
             )
         }
-        if result.count >= 3 {
-            result.insert(.divider(UUID().uuidString), at: 2)
+        if result.count >= dividerPosition {
+            result.insert(.divider(UUID().uuidString), at: dividerPosition)
         }
         return result
     }
